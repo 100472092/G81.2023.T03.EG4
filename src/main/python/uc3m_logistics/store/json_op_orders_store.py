73d01520b960d22dddd7077ccde0380f0af2a92a
100472092 <100472092@alumnos.uc3m.es>
2023-04-26 09:20:32 +0200

    singletone json_op_orders_store

--- src/main/python/uc3m_logistics/store/json_op_orders_store.py
+++ src/main/python/uc3m_logistics/store/json_op_orders_store.py
@@ -3,15 +3,28 @@ from uc3m_logistics.config.order_manager_config import JSON_FILES_PATH
 from uc3m_logistics.exception.order_management_exception import OrderManagementException
 
 
-class Json_op_order_store(Json_op):
-    def __init__(self):
-        self.path = JSON_FILES_PATH + "orders_store.json"
-        self.ip = "_OrderRequest__order_id"
-        self.data_list = None
+class Json_op_order_store():
+    class __Json_op_order_store(Json_op):
+        def __init__(self):
+            self.path = JSON_FILES_PATH + "orders_store.json"
+            self.ip = "_OrderRequest__order_id"
+            self.data_list = None
 
-    def save_order_id(self, data):
-        self.open()
-        if self.search(data.order_id) is not None:
-            raise OrderManagementException("order_id is already registered in orders_store")
-        self.data_list.append(data.__dict__)
-        self.save()
+        def save_order_id(self, data):
+            self.open()
+            if self.search(data.order_id) is not None:
+                raise OrderManagementException("order_id is already registered in orders_store")
+            self.data_list.append(data.__dict__)
+            self.save()
+
+    instance = None
+    def __new__(self):
+        if not Json_op_order_store.instance:
+            Json_op_order_store.instance = Json_op_order_store.__Json_op_order_store()
+        return Json_op_order_store.instance
+
+    def __getattr__(self, item):
+        return getattr(self.instance, item)
+
+    def __setattr__(self, key, value):
+        return setattr(self.instance, key, value)
