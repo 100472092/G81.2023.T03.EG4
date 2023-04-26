9bc16e7d0bda4e4ea541c23c0a0ee72ccdba6241
100472092 <100472092@alumnos.uc3m.es>
2023-04-26 09:36:24 +0200

    test singleton tenían un error. Ahora si que están bien

--- src/unittest/python/test_singleton_tests.py
+++ src/unittest/python/test_singleton_tests.py
@@ -15,14 +15,14 @@ class TestDeliverProduct(TestCase):
         """two instances that must be equal"""
         my_store = json_op_orders_delivered.Json_op_order_delivered()
         my_store1 = json_op_orders_delivered.Json_op_order_delivered()
-        self.assertEqual(my_store, my_store)
+        self.assertEqual(my_store, my_store1)
     def test_singletone_json_order_shiped(self):
         """two instances that must be equal"""
         my_store = json_op_orders_shiped.Json_op_order_shiped()
         my_store1 = json_op_orders_shiped.Json_op_order_shiped()
-        self.assertEqual(my_store, my_store)
+        self.assertEqual(my_store, my_store1)
     def test_singletone_json_order_store(self):
         """two instances that must be equal"""
         my_store = json_op_orders_store.Json_op_order_store()
         my_store1 = json_op_orders_store.Json_op_order_store()
-        self.assertEqual(my_store, my_store)
+        self.assertEqual(my_store, my_store1)
