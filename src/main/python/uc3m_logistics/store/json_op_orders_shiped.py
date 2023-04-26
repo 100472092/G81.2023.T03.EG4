6a700bb45b1229de2a9c3d5d2074b64e510ce506
100472092 <100472092@alumnos.uc3m.es>
2023-04-26 09:16:23 +0200

    singletone json_op_order_shiped

--- src/main/python/uc3m_logistics/store/json_op_orders_shiped.py
+++ src/main/python/uc3m_logistics/store/json_op_orders_shiped.py
@@ -1,13 +1,28 @@
 from .json_op import Json_op
 from uc3m_logistics.config.order_manager_config import JSON_FILES_PATH
 
-class Json_op_order_shiped(Json_op):
-    def __init__(self):
-        self.path = JSON_FILES_PATH + "shipments_store.json"
-        self.ip = ""
-        self.data_list = None
+class Json_op_order_shiped():
 
-    def save_shipments_delivered(self, shipment):
-        self.open()
-        self.data_list.append(shipment.__dict__)
-        self.save()
+    class __Json_op_order_shiped(Json_op):
+        def __init__(self):
+            self.path = JSON_FILES_PATH + "shipments_store.json"
+            self.ip = ""
+            self.data_list = None
+
+        def save_shipments_delivered(self, shipment):
+            self.open()
+            self.data_list.append(shipment.__dict__)
+            self.save()
+
+    instance = None
+
+    def __new__(self):
+        if not Json_op_order_shiped.instance:
+            Json_op_order_shiped.instance = Json_op_order_shiped.__Json_op_order_shiped()
+        return Json_op_order_shiped.instance
+
+    def __getattr__(self, item):
+        return getattr(self.instance, item)
+
+    def __setattr__(self, key, value):
+        return setattr(self.instance, key, value)
